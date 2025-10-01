2686e4b1556d423fb1a954d2393c1f1e0c53d32b
JesusGonzalez05 <Jesusgonzalez051698@gmail.com>
2025-10-01 19:59:15 -0400

    initialized array and added pointer created loop pointer will point toward the sum.

--- main.c
+++ main.c
@@ -1,10 +1,16 @@
 #include <stdio.h>
 
 int main (){
+    int arr[10];
+    int *p = arr;
 
-// initialize array values (1-9) and calculate sum
-
-
+    // initialize array values (1-9) and calculate sum
+    int sum = 0;
+    for (int i = 1; i <= 9; i++) {
+        *(p + i) = i;
+        sum += *(p + i);
+    }
+    *p = sum;  // arr[0] = sum
 
 // process 3 inputs
 
