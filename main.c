27db3ee73e82bdbb18144770d852493a3aa454ac
JesusGonzalez05 <Jesusgonzalez051698@gmail.com>
2025-10-01 21:36:48 -0400

    added loop that will process 3 inputs then change the pointer if parameters are met alos recalculate sum using loop.

--- main.c
+++ main.c
@@ -12,9 +12,22 @@ int main (){
     }
     *p = sum;  // arr[0] = sum
 
-// process 3 inputs
 
-// recalculate sum
+    // process 3 inputs
+    for (int i = 0; i < 3; i++) {
+        int value, pos;
+        scanf("%d %d", &value, &pos);
+
+        if (pos >= 1 && pos <= 9) {
+            *(p + pos) = value;  // pointer update
+        }
+    }
+   // recalculate sum
+    sum = 0;
+    for (int i = 1; i <= 9; i++) {
+        sum += *(p + i);
+    }
+    *p = sum;
 // print array
 
     return 0;
